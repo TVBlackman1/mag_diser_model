4d8311b155a2e8f4792658edbc8eab25efd83d6c
tvblackman1 <vbif.misha2012@yandex.ru>
2025-06-09 02:47:15 +0300

    fixes: remote wake up and mounting dirs

---
+++ boot/mount.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+LOCAL_PROJECT_DIR=$(pwd)
+LOCAL_IP=$(ipconfig getifaddr en0)
+LOCAL_USER=$(whoami)
+
+# 1. Wake-on-LAN
+echo "[1/4] Sending WOL to remote server..."
+wakeonlan -i "$BROADCAST_MASK_REMOTE_IP" "$REMOTE_MAC"
+
+# 2. Wait for host to be reachable
+echo "[2/4] Waiting for $REMOTE_IP to respond to ping..."
+while ! ping -c 1 -W 1 "$REMOTE_IP" &>/dev/null; do
+  sleep 1
+done
+echo "Host is up!"
+
+# 3. Connect over SSH (optional, can be skipped if just mounting)
+echo "[3/4] Verifying SSH access to $SSH_REMOTE_USER@$REMOTE_IP..."
+until ssh -o ConnectTimeout=2 "$SSH_REMOTE_USER@$REMOTE_IP" 'echo "SSH available"' 2>/dev/null; do
+  sleep 1
+done
+
+# 4. Remote SSHFS mount and open shell in mounted dir
+echo "[4/4] Mounting $LOCAL_PROJECT_DIR to $REMOTE_DIRECTORY and entering session..."
+
+ssh -t "$SSH_REMOTE_USER@$REMOTE_IP" "
+  export TERM=xterm-256color &&
+  mkdir -p '$REMOTE_DIRECTORY' &&
+  fusermount -u '$REMOTE_DIRECTORY' 2>/dev/null || true &&
+  /usr/bin/sshfs '$LOCAL_USER@$LOCAL_IP:$LOCAL_PROJECT_DIR' '$REMOTE_DIRECTORY' &&
+  cd '$REMOTE_DIRECTORY' &&
+  exec bash --login -i
+"
